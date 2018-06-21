6d5a902a275592c4a9bb599d836b30997fbf4e4a
chadz_graphen <chadzhuang@graphen.ai>
2018-06-22 00:41:37 +0800

    ngIf ngFor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,15 +9,22 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  // Interpolation
   title = `Todos`;
+
+  // [ Property ] Binding
   inputHolder = `What needs to be done`;
+
+  // Two-way [( ng-model )] Binding
   inputValue = `This is the input value from component`;
 
   ngOnInit() {
   }
+
+  // ( Event ) Binding
   handleClick(e) {
-    // console.log(e);
     this.title = e.target.value;
     console.log(this.inputValue);
   }
+
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Todoers {
+  labelText: string;
+  inputValue: string;
+  isChecked: boolean;
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
   constructor() { }
-  listTitle = `hello from component`;
-  ngOnInit() {
-  }
 
+  listTitle = `hello from component`;
+  todoItems: Todoers[] = [];
+  todoToggle = false;
+
+  ngOnInit() {
+    this.todoItems = [
+      {labelText: 'Taste JavaScript', inputValue: 'Create a TodoMVC template', isChecked: true},
+      {labelText: 'Buy a unicorn', inputValue: 'Rule the web', isChecked: false}
+    ];
+  }
+  handleItemChange() {
+    this.todoToggle = true;
+    setTimeout(() => {this.todoToggle = false; }, 1500);
+  }
 }
